4941b15dfb917cb59fa18667261278f5842804c4
Leandro Oliveira <leandro@agapsys.com>
2015-07-27 22:26:54 -0300

    Removed unused imports

--- src/main/java/com/agapsys/web/PersistenceModule.java
+++ src/main/java/com/agapsys/web/PersistenceModule.java
@@ -19,9 +19,6 @@ package com.agapsys.web;
 import com.agapsys.web.PersistenceUnit.DbInitializer;
 import com.agapsys.web.utils.Properties;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import org.hibernate.Session;
 
 /**
  * Persistence module
