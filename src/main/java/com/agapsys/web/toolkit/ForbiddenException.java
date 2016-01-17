d58d9e1bd03013da9e72a24581302b687f73dd42
Leandro Oliveira <leandro@agapsys.com>
2016-01-17 14:20:05 -0300

    Added new client exception

---
+++ src/main/java/com/agapsys/web/toolkit/ForbiddenException.java
@@ -0,0 +1,32 @@
+package com.agapsys.web.toolkit;
+
+
+import com.agapsys.web.toolkit.ClientException;
+
+/*
+ * Copyright 2016 Agapsys Tecnologia Ltda-ME.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class ForbiddenException extends ClientException {
+	// CLASS SCOPE =============================================================
+	public static final int CODE = 403;
+	// =========================================================================
+	
+	// INSTANCE SCOPE ==========================================================
+	public ForbiddenException(String message, Object...args) {
+		super(CODE, message, args);
+	}
+	// =========================================================================
+}
