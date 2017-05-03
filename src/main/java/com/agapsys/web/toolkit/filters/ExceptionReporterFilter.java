b9f46ddfbeac41b583ef79688fa091887063ed8e
Leandro Oliveira <leandro@agapsys.com>
2017-05-02 21:39:32 -0300

    Exception should not be suppressed

--- src/main/java/com/agapsys/web/toolkit/filters/ExceptionReporterFilter.java
+++ src/main/java/com/agapsys/web/toolkit/filters/ExceptionReporterFilter.java
@@ -29,31 +29,31 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 public class ExceptionReporterFilter implements Filter {
-    
+
     // <editor-fold desc="STATIC SCOPE" defaultstate="collapsed">
     private static ExceptionReporterFilter singleton = null;
-    
+
     private static void __setInstance(ExceptionReporterFilter instance) {
         synchronized(ExceptionReporterFilter.class) {
             singleton = instance;
         }
     }
-    
+
     public static ExceptionReporterFilter getInstance() {
         synchronized(ExceptionReporterFilter.class) {
             return singleton;
         }
     }
     // </editor-fold>
-    
+
     private ExceptionReporterService exceptionReporterService;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         __setInstance(this);
-        
+
         AbstractApplication app = (AbstractWebApplication) AbstractApplication.getRunningInstance();
-        
+
         if (app != null) {
             exceptionReporterService = app.getService(ExceptionReporterService.class, false);
         }
@@ -63,10 +63,12 @@ public class ExceptionReporterFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
             chain.doFilter(request, response);
-        } catch(Throwable ex) {
+        } catch(RuntimeException ex) {
             if (exceptionReporterService != null) {
                 exceptionReporterService.reportException(ex, (HttpServletRequest) request);
             }
+
+            throw ex;
         }
     }
 
