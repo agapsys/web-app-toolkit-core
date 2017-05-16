5490eacaff176551bad173a282fc57ddb0eb860e
Leandro Oliveira <leandro@agapsys.com>
2017-05-16 00:11:48 -0300

    Allows settings changes

--- src/main/java/com/agapsys/web/toolkit/services/SmtpService.java
+++ src/main/java/com/agapsys/web/toolkit/services/SmtpService.java
@@ -23,6 +23,7 @@ import com.agapsys.mail.SmtpSender;
 import com.agapsys.mail.SmtpSettings;
 import com.agapsys.web.toolkit.AbstractApplication;
 import com.agapsys.web.toolkit.Service;
+import java.util.Objects;
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
@@ -87,6 +88,18 @@ public class SmtpService extends Service {
         sender = null;
     }
 
+    private SmtpSender __getSmtpSender() {
+        SmtpSettings smtpSettings = new SmtpSettings();
+        smtpSettings.setServer(server);
+        smtpSettings.setAuthenticationEnabled(authEnabled);
+        smtpSettings.setUsername(username);
+        smtpSettings.setSecurityType(securityType);
+        smtpSettings.setPassword(getApplication().getProperty(KEY_PASSWORD, DEFAULT_PASSWORD));
+        smtpSettings.setPort(port);
+
+        return new SmtpSender(smtpSettings);
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -102,34 +115,47 @@ public class SmtpService extends Service {
             port         = app.getProperty(Integer.class, KEY_PORT, DEFAULT_PORT);
 
             securityType = SecurityType.valueOf(app.getProperty(KEY_SECURITY_TYPE, DEFAULT_SECURITY_TYPE.name()));
-
-            SmtpSettings smtpSettings = new SmtpSettings();
-            smtpSettings.setServer(server);
-            smtpSettings.setAuthenticationEnabled(authEnabled);
-            smtpSettings.setUsername(username);
-            smtpSettings.setSecurityType(securityType);
-            smtpSettings.setPassword(app.getProperty(KEY_PASSWORD, DEFAULT_PASSWORD));
-            smtpSettings.setPort(port);
-
-            smtpSender = new SmtpSender(smtpSettings);
             sender = __getSenderFromString(app.getProperty(KEY_SENDER, DEFAULT_SENDER));
         }
     }
 
-    /**
-     * Returns the sender address defined in application settings.
-     *
-     * @return sender address.
-     */
+
     public InternetAddress getSender() {
         synchronized(this) {
             return sender;
         }
     }
+    public void setSender(InternetAddress sender) {
+        synchronized(this) {
+            if (sender == null)
+                throw new IllegalArgumentException("Null sender");
+
+            if (!Objects.equals(this.sender, sender)) {
+                this.sender = sender;
+                getApplication().setProperty(KEY_SENDER, sender.toString());
+                this.smtpSender = null;
+            }
+        }
+    }
+    public void setSender(String sender) throws AddressException {
+        setSender(new InternetAddress(sender));
+    }
 
     public String getServer() {
         synchronized(this) {
             return server;
+        }
+    }
+    public void setServer(String server) {
+        synchronized(this) {
+            if (server == null || server.isEmpty())
+                throw new IllegalArgumentException("Null/Empty server");
+
+            if (!Objects.equals(this.server, server)) {
+                this.server = server;
+                getApplication().setProperty(KEY_SERVER, server);
+                this.smtpSender = null;
+            }
         }
     }
 
@@ -138,10 +164,38 @@ public class SmtpService extends Service {
             return authEnabled;
         }
     }
+    public void setAuthEnabled(boolean authEnabled) {
+        synchronized(this) {
+            if (this.authEnabled != authEnabled) {
+                this.authEnabled = authEnabled;
+                getApplication().setProperty(KEY_AUTH_ENABLED, authEnabled);
+                this.smtpSender = null;
+            }
+        }
+    }
 
     public String getUsername() {
         synchronized(this) {
             return username;
+        }
+    }
+    public void setUsername(String username) {
+        synchronized(this) {
+            if (username == null)
+                username = "";
+
+            if (!Objects.equals(this.username, username)) {
+                this.username = username;
+                getApplication().setProperty(KEY_USERNAME, username);
+                this.smtpSender = null;
+            }
+        }
+    }
+
+    public void setPassword(String password) {
+        synchronized(this) {
+            getApplication().setProperty(KEY_PASSWORD, password);
+            this.smtpSender = null;
         }
     }
 
@@ -150,10 +204,31 @@ public class SmtpService extends Service {
             return securityType;
         }
     }
+    public void setSecurityType(SecurityType securityType) {
+        synchronized(this) {
+            if (securityType == null)
+                throw new IllegalArgumentException("Null security type");
+
+            if (this.securityType != securityType) {
+                this.securityType = securityType;
+                getApplication().setProperty(KEY_SECURITY_TYPE, securityType.name());
+                this.smtpSender = null;
+            }
+        }
+    }
 
     public int getPort() {
         synchronized(this) {
             return port;
+        }
+    }
+    public void setPort(int port) {
+        synchronized(this) {
+            if (this.port != port) {
+                this.port = port;
+                getApplication().setProperty(KEY_PORT, port);
+                this.smtpSender = null;
+            }
         }
     }
 
@@ -179,6 +254,9 @@ public class SmtpService extends Service {
                     .setSubject(message.getSubject())
                     .setText(message.getText()).build();
             }
+
+            if (smtpSender == null)
+                smtpSender = __getSmtpSender();
 
             smtpSender.sendMessage(message);
         }
